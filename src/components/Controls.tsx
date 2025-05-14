d967a2d4e129090f996db885677ceba43437ea02
tinxte <150742495+Tinxte@users.noreply.github.com>
2025-05-13 21:25:06 -0500

    added directional control buttons

---
+++ src/components/Controls.tsx
@@ -0,0 +1,20 @@
+import { queueMove } from "../store/player";
+// import { useEventListeners } from "../hooks/useEventListeners"
+import "./Controls.css";
+
+export function Controls() {
+    // TODO create event listeners 3250
+
+    // useEventListeners();
+
+    return (
+        <div id="controls">
+            <div>
+                <button onClick={() => queueMove("forward")}>↑</button>
+                <button onClick={() => queueMove("left")}>←</button>
+                <button onClick={() => queueMove("backward")}>↓</button>
+                <button onClick={() => queueMove("right")}>→</button>
+            </div>
+        </div>
+    );
+}
